02b7ce15771986924cdf3cac303939d3463d25ba
Oscar Hernandez <ooscar1995@gmail.com>
2020-09-22 16:57:35 -0500

    Avance JWT

--- pruebaTecnica/src/main/java/co/prueba/app/controller/UserController.java
+++ pruebaTecnica/src/main/java/co/prueba/app/controller/UserController.java
@@ -1,10 +1,21 @@
 package co.prueba.app.controller;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
 import java.util.Date;
 import java.util.List;
 import java.util.stream.Collectors;
 
-import org.springframework.http.MediaType;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,21 +24,33 @@ import org.springframework.web.bind.annotation.RestController;
 
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
-
 import co.prueba.app.model.dto.User;
 
-@RestController
+@RestController // para login
 public class UserController {
 
 	private final long TIEMPO_TOKEN = 300000;
 	private final String SECRET = "miLlaveSecreta";
 	private final String PREFIX = "miToken ";
 
-	@PostMapping(value = "login")
-	public User login(@RequestParam("user") String username, @RequestParam("password") String pwd) {
 
-		//TO DO validar desde una lista 
-		String token = getJWTToken(username);	
+	@PostMapping(value = "login")
+	public User login(@RequestParam("user") String username, @RequestParam("password") String pwd)
+			throws IOException, ParseException {
+
+		JSONParser parser = new JSONParser();
+		Resource resource = new ClassPathResource("static/usuarios.json");
+
+		// TO DO validar desde una lista
+		if (!username.equals("oscar")) {
+			return null;
+		}
+		if (!pwd.equals("mipassword")) {
+			return null;
+		}
+		System.out.println("Se Logueo");
+		// TO DO validar desde una lista
+		String token = getJWTToken(username);
 		User user = new User();
 		user.setUsername(username);
 		user.setToken(token);
@@ -47,4 +70,5 @@ public class UserController {
 
 		return PREFIX + token;
 	}
+
 }
