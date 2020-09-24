352a812f9fdb4daeb6808a3ed3e419f4dce2fc52
Oscar Hernandez <ooscar1995@gmail.com>
2020-09-23 21:09:36 -0500

    Configuracion Docker

--- pruebaTecnica/dockerInstal.bat
+++ pruebaTecnica/dockerInstal.bat
@@ -1,12 +1,13 @@
 
-call mvnw clean install
+call mvnw clean
+call mvnw install -Dmaven.test.skip=true
+
 
 copy application.properties target/application.properties
 
-call docker stop sesiones-systig
-call docker rm sesiones-systig
+call docker stop prueba
+call docker rm prueba
 
-call docker build -t sesiones .
 
-call docker run -d -p 8080:8080 --name sesiones-systig sesiones
-stop
+call docker build -t pruebatecnica .
+call docker run -d -p 8080:8080 --name prueba pruebatecnica
